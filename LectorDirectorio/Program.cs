50ed2b247a437489a84aa0b78aa915aa9c8948bd
AltamiranoSantiago <santy4lt@gmail.com>
2025-06-10 12:35:18 -0300

    Obtencion de la ruta especificada por el usuario

---
+++ LectorDirectorio/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+string path;
+Console.WriteLine("Ingrese el directorio a analizar: ");
+path = Console.ReadLine();
+while (!Directory.Exists(path))
+{
+    Console.WriteLine("El directorio indicado no existe, ingrese un directorio valido");
+    path = Console.ReadLine();
+}
+Console.WriteLine("-------------------------------------------------------------------\n");
+Console.WriteLine("Directorio elegido:");
+Console.WriteLine(path + "\n");
+Console.WriteLine("-------------------------------------------------------------------");
